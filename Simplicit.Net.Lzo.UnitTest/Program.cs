6d54ebf0ecf7e9504db1bec3fc8fe0dc3d25aa43
Derek Liang <Derek.Liang.Ca@gmail.com>
2011-12-13 17:35:33 -0800

    initial check-in

---
+++ Simplicit.Net.Lzo.UnitTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simplicit.Net.Lzo.UnitTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create the compressor object
+			LZOCompressor lzo = new LZOCompressor();
+
+			// Build a quite redundant string
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 10000; i++)
+			{
+				sb.Append("LZO.NET");
+			}
+			string str = sb.ToString();
+			Console.WriteLine("Original-Length: " + str.Length);
+
+			// Now compress the 70000 byte string to something much smaller
+			byte[] compressed = lzo.Compress(Encoding.Default.GetBytes(str));
+			Console.WriteLine("Compressed-Length: " + compressed.Length);
+
+			// Decompress the string to its original content
+			string str2 = Encoding.Default.GetString(lzo.Decompress(compressed));
+			Console.WriteLine("Decompressed-Length: " + str2.Length);
+			Console.WriteLine("Equality: " + str.Equals(str2));
+		}
+	}
+}
